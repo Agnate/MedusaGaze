53daecc611b931759f064025e8319e064a5e25c9
Paul Dinelle <pauldinelle@gmail.com>
2011-07-27 13:14:37 -0400

    First commit!

---
+++ src/ca/agnate/medusagaze/MedusaGaze.java
@@ -0,0 +1,16 @@
+package ca.agnate.medusagaze;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MedusaGaze extends JavaPlugin {
+    public void onDisable() {
+        // TODO: Place any custom disable code here.
+        System.out.println(this + " is now disabled!");
+    }
+
+    public void onEnable() {
+        // TODO: Place any custom enable code here, such as registering events
+        
+        System.out.println(this + " is now enabled!");
+    }
+}
